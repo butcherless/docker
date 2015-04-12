757e580a8ea6128da389a9ca7f7bc744fdcefe10
Carlos Martin <cmartin@gmail.com>
2015-04-12 13:10:55 +0200

    docker aliases; jenkins image

--- run-tutorial.sh
+++ run-tutorial.sh
@@ -11,19 +11,18 @@ clear
 
 # rm -rf /tmp/docker-tutorial
 # mkdir /tmp/docker-tutorial
-cd /tmp/docker-tutorial
+# cd /tmp/docker-tutorial
 
-echo -e "cloning git repo"
+echo -e "cloning git repo..."
 # git clone https://github.com/butcherless/docker
+# cd docker
 
-exit 1
-
-cd docker/ubuntu/base-image
+cd ubuntu/base-image
 echo -e "\ncurrent dir: `pwd`"
 pause ${PRESS_ENTER_MSG}
 
 echo -e "\nlisting ubuntu images..."
-sleep 2
+sleep 1
 
 sudo docker images | grep ubuntu
 pause ${PRESS_ENTER_MSG}
@@ -32,10 +31,9 @@ pause ${PRESS_ENTER_MSG}
 # sudo docker rmi ubuntu/base
 
 echo -e "\nbuilding ubuntu base image..."
-sleep 2
 
 sudo docker build -t ubuntu/base .
-ssh-keygen -f "/home/cmartin/.ssh/known_hosts" -R [localhost]:2222
+ssh-keygen -qf "/home/cmartin/.ssh/known_hosts" -R [localhost]:2222
 sudo docker run -d -p 2222:22 --name mysshd -h ssh-server ubuntu/base
 sudo docker ps -a
 sudo docker port mysshd 22
@@ -45,47 +43,46 @@ sudo docker exec mysshd hostname
 pause "after press enter you can type some commands and finally, press CTRL+D o type exit"
 ssh -i mykey root@localhost -p 2222 -oStrictHostKeyChecking=no
 
-echo -e "\nstopping and removing container 'mysshd'"
+echo -e "\nstopping container 'mysshd'..."
 sudo docker stop mysshd
+echo -e "\nremoving container 'mysshd'..."
 sudo docker rm mysshd
 
 pause "end base image step!"
 
 # Java 8 image
 clear
+
+echo -e "\nbuilding ubuntu java 8 image..."
+
 cd ../java8
 echo -e "\ncurrent dir: `pwd`"
 pause ${PRESS_ENTER_MSG}
 
-echo -e "\nbuilding ubuntu java8 image..."
-sleep 2
+echo -e "\nbuilding ubuntu java 8 image..."
+sleep 1
 
 sudo docker build -t ubuntu/java8 .
 
 echo -e "\nchecking java se installation..."
-sleep 2
+sleep 1
 sudo docker run --rm ubuntu/java8 java -version
 
 echo -e "\nchecking maven installation..."
-sleep 2
+sleep 1
 sudo docker run --rm ubuntu/java8 mvn -v
 
 echo -e "\nchecking gradle installation..."
-sleep 2
+sleep 1
 sudo docker run --rm ubuntu/java8 gradle -v
 
-exit 1
+pause "end java 8 image step!"
+
+# Java 8 image
+clear
+
+echo -e "\nbuilding ubuntu jenkins image..."
+
 
 cd ../jenkins
 sudo docker build -t ubuntu/jenkins .
-
-
-
-# docker aliases
-
-alias dkrmdi='sudo docker rmi $(sudo docker images -qf dangling=true)'
-alias dklsi='sudo docker images'
-alias dklsc='sudo docker ps -a'
-alias dkrmc='sudo docker rm'
-alias dkrmcs='sudo docker rm $(dklsc -q)'
-alias dkrc='sudo docker run --rm'
